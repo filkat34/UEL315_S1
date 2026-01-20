a44379a723b20140a55abdc37e7ac07aac0e57d4
mathildechauvet <mathildechauvet@outlook.fr>
2026-01-20 13:21:35 +0100

    ex3 req7 commentaire

--- semaine3/Exercice 3/req7.mongodb.js
+++ semaine3/Exercice 3/req7.mongodb.js
@@ -2,6 +2,7 @@
 use("UEL315_S3");
 
 // Afficher les années des publications de l'auteur « Wolfgang Wahlster »
+// Pas de doublons + trie par ordre croissant côté JS
 const years = db.getCollection("dblp").distinct("year", {
   $or: [{ authors: "Wolfgang Wahlster" }, { author: "Wolfgang Wahlster" }],
 });
